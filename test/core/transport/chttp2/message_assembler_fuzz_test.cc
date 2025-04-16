1fcd68e93b8d9a48fb29dd09c09f3937bc68a460
tanvi-jagtap <139093547+tanvi-jagtap@users.noreply.github.com>
2025-04-16 04:44:51 +0000

    Automated change: Fix sanity tests

--- test/core/transport/chttp2/message_assembler_fuzz_test.cc
+++ test/core/transport/chttp2/message_assembler_fuzz_test.cc
@@ -46,6 +46,6 @@ namespace testing {
 }  // namespace grpc_core
 
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+  grpc_core::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
