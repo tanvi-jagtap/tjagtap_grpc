a7683bff31cf2483945a5d76a2a0ef23417f5736
tanvi-jagtap <139093547+tanvi-jagtap@users.noreply.github.com>
2025-04-24 04:54:36 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chttp2/transport/message_assembler.h
+++ src/core/ext/transport/chttp2/transport/message_assembler.h
@@ -55,7 +55,7 @@ class GrpcMessageAssembler {
     if constexpr (sizeof(size_t) <= 4) {
       if (message_buffer_.Length() < UINT32_MAX - payload.Length()) {
         // STREAM_ERROR
-        return absl::Status::InternalError(
+        return absl::InternalError(
             "Stream Error: SliceBuffer overflow for 32 bit platforms.");
       }
     }
@@ -73,7 +73,7 @@ class GrpcMessageAssembler {
     GrpcMessageHeader header = ExtractGrpcHeader(message_buffer_);
     if constexpr (sizeof(size_t) <= 4) {
       if (header.length > kOneGb) {
-        return absl::Status::InternalError(
+        return absl::InternalError(
             "Stream Error: SliceBuffer overflow for 32 bit platforms.");
       }
     }
